d9778400d823086b2b9aa54f578c5da93b17fd44
Prabhdeep Singh <prabhdeepsingh0007@gmail.com>
2023-08-28 18:05:47 +0530

    .

--- contactus.js
+++ contactus.js
@@ -1,17 +1,17 @@
 function sendMail() {
     var params = {
-      name: document.getElementById("name").value,
+      namee: document.getElementById("namee").value,
       email: document.getElementById("email").value,
       message: document.getElementById("message").value,
     };
-  if(name==""|| email=="" || message=="")  alert("All FIELDS ARE MANDATORY")
+  if(namee==""|| email=="" || message=="")  alert("All FIELDS ARE MANDATORY")
   else{
     const serviceID = "service_2v7uwue";
     const templateID = "template_mmbrn63";
   
       emailjs.send(serviceID, templateID, params)
       .then(res=>{
-          document.getElementById("name").value = "";
+          document.getElementById("namee").value = "";
           document.getElementById("email").value = "";
           document.getElementById("message").value = "";
           console.log(res);
